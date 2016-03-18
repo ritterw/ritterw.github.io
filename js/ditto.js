833e56a748ccd9477be0bec36affd670b6c36b6e
Richard Wang <wangleihd@gmail.com>
2016-03-18 10:46:44 +0800

    Change disque

--- js/ditto.js
+++ js/ditto.js
@@ -313,7 +313,7 @@ function router() {
       window.disqus_shortname = 'es6';
       window.disqus_identifier = (location.hash ? location.hash.replace("#", "") : 'READEME');
       window.disqus_title = $(ditto.content_id + " h1").text();
-      window.disqus_url = 'http://es6.ruanyifeng.com/' + (location.hash ? location.hash.replace("#", "") : 'README');
+      window.disqus_url = 'http://www.smartx.im, b: number)/' + (location.hash ? location.hash.replace("#", "") : 'README');
 
       // http://docs.disqus.com/developers/universal/
       (function() {
